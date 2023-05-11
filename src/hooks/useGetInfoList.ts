29cac12a8fb6298e6de109ba0bb1cb97be424b72
kim-limit <rlawogks2468@naver.com>
2023-05-11 19:08:13 +0900

    refactor: 주석 추가

--- src/hooks/useGetInfoList.ts
+++ src/hooks/useGetInfoList.ts
@@ -3,6 +3,10 @@ import { INFO_LIST } from "../mock";
 import { getInfoList } from "@/services/info";
 import { queryKeys } from "@/react-query/constants";
 
+/**
+ * 현재 로그인한 기사의 택배 리스트 불러오는 hook
+ * @returns info 리스트
+ */
 export const useGetInfoList = () => {
   const {
     // data = INFO_LIST,
