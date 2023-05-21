fdafb84360f807100dd2ebc8cc176b7e4f1fdfc8
kim-limit <rlawogks2468@naver.com>
2023-05-21 22:03:02 +0900

    feat: 스캔 로직 추가

--- src/hooks/useGetScanResult.ts
+++ src/hooks/useGetScanResult.ts
@@ -1,26 +1,41 @@
 import { useQuery } from "@tanstack/react-query";
-import { ScanType } from "@/types/scan";
 import { getScanResult } from "@/services/scan";
 import { queryKeys } from "@/react-query/constants";
 import { INFO_LIST } from "../mock";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 /**
  * 스캔 결과값을 받아오는 hooks, 만약 값 있다? 값 반환, 유효하지않은 qr, 니꺼아님
  * @params 스캔한 코드 값
  * @returns info 리스트
  */
-export const useGetScanResult = (props: ScanType) => {
+export const useGetScanResult = () => {
+  const [result, setResult] = useState("");
+  const navigate = useNavigate();
+
   const {
-    // data = INFO_LIST[0],
+    data = INFO_LIST[0],
     // refetch,
     // isLoading,
     // isSuccess,
     // isError,
-  } = useQuery(queryKeys.scan, () => getScanResult(props), {
+  } = useQuery(queryKeys.scan, () => getScanResult({ code: result }), {
     // enabled로 code값이 있을때만 요청하도록
     enabled: false,
     // !!props.code,
   });
+
+  const handleScan = (result: string | null) => {
+    if (result) {
+      setResult(result);
+      navigate("/scan/result", {
+        state: {
+          infoData: data,
+        },
+      });
+    }
+  };
 
   // todo isLoading -> isSuccess
   // useEffect(() => {
@@ -34,5 +49,5 @@ export const useGetScanResult = (props: ScanType) => {
   //   }
   // }, [isLoading, props.code]);
 
-  return { data: INFO_LIST[0] };
+  return { data: INFO_LIST[0], handleScan, result };
 };
--- src/pages/Scan.tsx
+++ src/pages/Scan.tsx
@@ -1,26 +1,11 @@
-import { useState } from "react";
 import ScanTemplate from "@/components/template/scan";
 import { useGetScanResult } from "@/hooks/useGetScanResult";
-import { useNavigate } from "react-router-dom";
 
 const Scan = () => {
-  const [result, setResult] = useState("");
-  const navigate = useNavigate();
-
-  const { data } = useGetScanResult({ code: result });
+  const { result, handleScan } = useGetScanResult();
 
   const handleError = (error: any) => {
     console.log(error);
-  };
-  const handleScan = (result: string | null) => {
-    if (result) {
-      setResult(result);
-      navigate("/scan/result", {
-        state: {
-          infoData: data,
-        },
-      });
-    }
   };
 
   return (
